a2eac69ece550fc0517b5b737080a8b857354dcb
Junxiao Shi <git@mail1.yoursunny.com>
2025-08-07 15:34:58 +0000

    examples: parallelism in 3_nodes_thruput

--- examples/3_nodes_thruput.py
+++ examples/3_nodes_thruput.py
@@ -1,3 +1,7 @@
+import itertools
+from multiprocessing import Pool, freeze_support
+from typing import Any
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -8,25 +12,16 @@ from qns.network.protocol import ProactiveForwarder
 from qns.simulator import Simulator
 from qns.utils import log, set_seed
 
-from examples_common.stats import gather_etg_decoh
 from examples_common.topo_3_nodes import build_topology
-
-
-# Command line arguments
-class Args(Tap):
-    runs: int = 100  # number of trials per parameter set
-    csv: str = ""  # save results as CSV file
-    plt: str = ""  # save plot as image file
-
-
-args = Args().parse_args()
 
 log.set_default_level("CRITICAL")
 
+# Constants
 SEED_BASE = 100
-
-# parameters
 sim_duration = 3
+
+# Experiment parameters
+t_cohere_values = [0.002, 0.005, 0.01, 0.015, 0.02, 0.025, 0.05, 0.1]
 
 
 def run_simulation(t_coherence: float, seed: int):
@@ -64,60 +59,75 @@ def run_simulation(t_coherence: float, seed: int):
     s.run()
 
     #### get stats
-    _, _, decoh_ratio = gather_etg_decoh(net)
     e2e_rate = net.get_node("S").get_app(ProactiveForwarder).cnt.n_consumed / sim_duration
-    return e2e_rate, decoh_ratio
+    return e2e_rate
 
 
-########################### Main #########################
-results = {"T_cohere": [], "Mean Rate": [], "Std Rate": []}
-
-t_cohere_values = [0.002, 0.005, 0.01, 0.015, 0.02, 0.025, 0.05, 0.1]
-
-for t_cohere in t_cohere_values:
-    rates = []
-    for i in range(args.runs):
+def run_row(n_runs: int, t_cohere: float) -> tuple[float, list[float]]:
+    rates: list[float] = []
+    for i in range(n_runs):
         print(f"T_cohere={t_cohere:.4f}, run {i + 1}")
-        seed = SEED_BASE + i
-        rate, *_ = run_simulation(t_cohere, seed)
+        rate = run_simulation(t_cohere, SEED_BASE + i)
         rates.append(rate)
-
-    results["T_cohere"].append(t_cohere)
-    results["Mean Rate"].append(np.mean(rates))
-    results["Std Rate"].append(np.std(rates))
+    return t_cohere, rates
 
 
-# Final results summary print
-print("\nT_coh    Rate")
-for t, mean, std in zip(results["T_cohere"], results["Mean Rate"], results["Std Rate"]):
-    print(f"{t:<7.3f}  {mean:>5.1f} ({std:.1f})")
+def save_results(results: Any, *, save_csv: str | None, save_plt: str | None):
+    # Final results summary print
+    print("\nT_coh    Rate")
+    for t, mean, std in zip(results["T_cohere"], results["Mean Rate"], results["Std Rate"]):
+        print(f"{t:<7.3f}  {mean:>5.1f} ({std:.1f})")
+
+    # Convert to DataFrame
+    df = pd.DataFrame(results)
+    if save_csv:
+        df.to_csv(save_csv, index=False)
+
+    # Plotting
+    plt.figure(figsize=(6, 4))
+    plt.errorbar(
+        df["T_cohere"],
+        df["Mean Rate"],
+        yerr=df["Std Rate"],
+        fmt="o",
+        color="orange",
+        ecolor="orange",
+        capsize=4,
+        label="sim.",
+        linestyle="--",
+    )
+    plt.xscale("log")
+    plt.xlabel(r"$T_{\mathrm{cohere}}$")
+    plt.ylabel("Ent. per second")
+    plt.title("E2e rate")
+    plt.legend()
+    plt.grid(True, which="both", ls="--", lw=0.5)
+    plt.tight_layout()
+    if save_plt:
+        plt.savefig(save_plt, dpi=300, transparent=True)
+    plt.show()
 
 
-# Convert to DataFrame
-df = pd.DataFrame(results)
-if args.csv:
-    df.to_csv(args.csv, index=False)
+if __name__ == "__main__":
+    freeze_support()
 
-# Plotting
-plt.figure(figsize=(6, 4))
-plt.errorbar(
-    df["T_cohere"],
-    df["Mean Rate"],
-    yerr=df["Std Rate"],
-    fmt="o",
-    color="orange",
-    ecolor="orange",
-    capsize=4,
-    label="sim.",
-    linestyle="--",
-)
-plt.xscale("log")
-plt.xlabel(r"$T_{\mathrm{cohere}}$")
-plt.ylabel("Ent. per second")
-plt.title("E2e rate")
-plt.legend()
-plt.grid(True, which="both", ls="--", lw=0.5)
-plt.tight_layout()
-if args.plt:
-    plt.savefig(args.plt, dpi=300, transparent=True)
-plt.show()
+    # Command line arguments
+    class Args(Tap):
+        workers: int = 1  # number of workers for parallel execution
+        runs: int = 100  # number of trials per parameter set
+        csv: str = ""  # save results as CSV file
+        plt: str = ""  # save plot as image file
+
+    args = Args().parse_args()
+
+    # Simulator loop with process-based parallelism
+    with Pool(processes=args.workers) as pool:
+        rows = pool.starmap(run_row, itertools.product([args.runs], t_cohere_values))
+
+    results = {"T_cohere": [], "Mean Rate": [], "Std Rate": []}
+    for t_cohere, rates in rows:
+        results["T_cohere"].append(t_cohere)
+        results["Mean Rate"].append(np.mean(rates))
+        results["Std Rate"].append(np.std(rates))
+
+    save_results(results, save_csv=args.csv, save_plt=args.plt)
