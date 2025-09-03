e2e6d62dcc0185895d054e33a9e98a5eb0b3485e
Junxiao Shi <git@mail1.yoursunny.com>
2025-09-03 17:42:15 +0000

    qchannel: adjust formulas and test with tau_0

--- test/entity/test_link_arch.py
+++ test/entity/test_link_arch.py
@@ -2,29 +2,45 @@ import pytest
 
 from qns.entity.qchannel import LinkArch, LinkArchDimBk, LinkArchSim, LinkArchSr
 
+DurationMultipliers = tuple[float, float]
+"""
+Link architecture duration multiplier: (m_l, m_0).
+The duration is: (m_l * tau_l + m_0 * tau_0).
+"""
 
-def check_link_arch(link_arch: LinkArch, *, attempt_duration: float, epr_creation: float, notify_a: float, notify_b: float):
-    tau = 0.000471
 
-    d1_epr_creation, d1_notify_a, d1_notify_b = link_arch.delays(1, reset_time=0.0, tau_l=tau, tau_0=0.0)
-    assert d1_epr_creation == pytest.approx(epr_creation * tau, abs=1e-6)
-    assert d1_notify_a == pytest.approx(notify_a * tau, abs=1e-6)
-    assert d1_notify_b == pytest.approx(notify_b * tau, abs=1e-6)
+def check_link_arch(
+    link_arch: LinkArch,
+    *,
+    attempt_duration: DurationMultipliers,
+    epr_creation: DurationMultipliers = (0, 0),
+    notify_a: DurationMultipliers,
+    notify_b: DurationMultipliers,
+):
+    tau_l, tau_0 = 0.000471, 0.000031
 
-    d6_epr_creation, _, _ = link_arch.delays(6, reset_time=0.0, tau_l=tau, tau_0=0.0)
-    assert d6_epr_creation - d1_epr_creation == pytest.approx(5 * attempt_duration * tau, abs=1e-6)
+    def compare_to(m: DurationMultipliers, m2=1.0):
+        return pytest.approx((tau_l * m[0] + tau_0 * m[1]) * m2, abs=1e-6)
+
+    d1_epr_creation, d1_notify_a, d1_notify_b = link_arch.delays(1, reset_time=0.0, tau_l=tau_l, tau_0=tau_0)
+    assert d1_epr_creation == compare_to(epr_creation)
+    assert d1_notify_a == compare_to(notify_a)
+    assert d1_notify_b == compare_to(notify_b)
+
+    d6_epr_creation, _, _ = link_arch.delays(6, reset_time=0.0, tau_l=tau_l, tau_0=tau_0)
+    assert d6_epr_creation - d1_epr_creation == compare_to(attempt_duration, 5)
 
 
 def test_dim_bk():
     link_arch = LinkArchDimBk()
-    check_link_arch(link_arch, attempt_duration=2, epr_creation=0, notify_a=2, notify_b=2)
+    check_link_arch(link_arch, attempt_duration=(2, 2), notify_a=(2, 2), notify_b=(2, 2))
 
 
 def test_sr():
     link_arch = LinkArchSr()
-    check_link_arch(link_arch, attempt_duration=2, epr_creation=0, notify_a=1, notify_b=2)
+    check_link_arch(link_arch, attempt_duration=(2, 1), notify_a=(1, 1), notify_b=(2, 1))
 
 
 def test_sim():
     link_arch = LinkArchSim()
-    check_link_arch(link_arch, attempt_duration=1, epr_creation=0, notify_a=1, notify_b=1)
+    check_link_arch(link_arch, attempt_duration=(1, 1), notify_a=(1, 1), notify_b=(1, 1))
